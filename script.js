65e48fd7c5e867879f0d70209d6ab785e9642249
kaushal patil <patilkaushl925@gmail.com>
2023-06-20 10:33:27 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function redirectToHome() {
+  alert("Login Successful!");
+  window.location.href = "http://127.0.0.1:5500/index.html";
+}
+function redirecttologin() {
+  window.location.href = "http://127.0.0.1:5500/login.html";
+}
