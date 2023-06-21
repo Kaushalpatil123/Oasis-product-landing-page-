276faa6d27149563a09c1f5eda95ddee3db27e3a
kaushal patil <91415972+Kaushalpatil123@users.noreply.github.com>
2023-06-20 22:16:03 -0700

    Update script.js sc

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function redirectToHome() {
   alert("Login Successful!");
-  window.location.href = "https://kaushalpatil123.github.io/Oasis-product-landing-page-/";
+  window.location.href = " https://kaushalpatil123.github.io/Oasis-product-landing-page-/ ";
 }
 
